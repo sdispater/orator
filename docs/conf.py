9cd4ab01eb28bd3f72e663f44e49f314123a53d5
Sébastien Eustace <sebastien.eustace@gmail.com>
2015-03-26 18:54:07 -0500

    Updated docs

--- docs/conf.py
+++ docs/conf.py
@@ -100,23 +100,7 @@ pygments_style = 'pastie'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    html_theme = 'sphinx_rtd_theme'
-    # Override default css to get a larger width for local build
-    def setup(app):
-        #app.add_javascript("custom.js")
-        app.add_stylesheet('custom.css')
-else:
-    # Override default css to get a larger width for ReadTheDoc build
-    html_context = {
-        'css_files': [
-            'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
-            'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
-            '_static/custom.css',
-        ],
-    }
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -274,3 +258,19 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:
+    html_theme = 'sphinx_rtd_theme'
+    def setup(app):
+        app.add_stylesheet('theme_overrides.css')
+else:
+    html_context = {
+        'css_files': [
+            'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
+            'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
+            '_static/theme_overrides.css',
+        ],
+    }
