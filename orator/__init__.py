746b535ae2bd2b2a9535d19ca3a596507c15b0a2
Sébastien Eustace <sebastien.eustace@gmail.com>
2015-05-25 16:24:20 -0500

    Makes Paginator and LengthAwarePaginator available at root

--- orator/__init__.py
+++ orator/__init__.py
@@ -4,3 +4,4 @@ from .orm import Model, SoftDeletes, Collection
 from .database_manager import DatabaseManager
 from .query.expression import QueryExpression
 from .schema import Schema
+from .pagination import Paginator, LengthAwarePaginator
