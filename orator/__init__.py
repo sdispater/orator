9527eebd38a30757338d57e44f449f2a831509f7
Sébastien Eustace <sebastien.eustace@gmail.com>
2015-05-30 00:22:08 -0500

    Majes accessor and mutator decorators more accessible

--- orator/__init__.py
+++ orator/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .orm import Model, SoftDeletes, Collection
+from .orm import Model, SoftDeletes, Collection, accessor, mutator
 from .database_manager import DatabaseManager
 from .query.expression import QueryExpression
 from .schema import Schema
