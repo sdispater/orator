cc02c0c49f8d3a4827f67b6089b392f42ff95a3c
Sébastien Eustace <sebastien.eustace@gmail.com>
2015-10-05 16:42:20 -0500

    Adds more utils

--- orator/utils/__init__.py
+++ orator/utils/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
+import errno
 
 PY2 = sys.version_info[0] == 2
 PY3K = sys.version_info[0] >= 3
@@ -55,14 +57,46 @@ class Null(object):
         return other is None
 
 
-def decode(s, encodings=('utf8', 'ascii', 'latin1')):
-    if not PY2:
-        return s
+def decode(string, encodings=None):
+    if not PY2 and not isinstance(string, bytes):
+        return string
+
+    if encodings is None:
+        encodings = ['utf-8', 'latin1', 'ascii']
 
     for encoding in encodings:
         try:
-            return s.decode(encoding)
+            return string.decode(encoding)
         except UnicodeDecodeError:
             pass
 
-    return s.decode('utf8', 'ignore')
+    return string.decode(encodings[0], errors='ignore')
+
+
+def encode(string, encodings=None):
+    if not PY2 and isinstance(string, bytes):
+        return string
+
+    if PY2 and isinstance(string, unicode):
+        return string
+
+    if encodings is None:
+        encodings = ['utf-8', 'latin1', 'ascii']
+
+    for encoding in encodings:
+        try:
+            return string.encode(encoding)
+        except UnicodeDecodeError:
+            pass
+
+    return string.encode(encodings[0], errors='ignore')
+
+
+def mkdir_p(path, mode=0o777):
+    try:
+        os.makedirs(path, mode)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
