9892fdf0a2334c509d8ab8d95204d27c0073fe51
Sébastien Eustace <sebastien.eustace@gmail.com>
2015-12-23 17:16:13 -0500

    Improving fluents

---
+++ tests/support/test_fluent.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from .. import OratorTestCase
+from orator.support.fluent import Fluent
+
+
+class FluentTestCase(OratorTestCase):
+
+    def test_get_method_return_attributes(self):
+        fluent = Fluent(name='john')
+
+        self.assertEqual('john', fluent.get('name'))
+        self.assertEqual('default', fluent.get('foo', 'default'))
+        self.assertEqual('john', fluent.name)
+        self.assertEqual(None, fluent.foo)
+
+    def test_set_attributes(self):
+        fluent = Fluent()
+
+        fluent.name = 'john'
+        fluent.developer()
+        fluent.age(25)
+
+        self.assertEqual('john', fluent.name)
+        self.assertTrue(fluent.developer)
+        self.assertEqual(25, fluent.age)
+
+        self.assertEqual({'name': 'john', 'developer': True, 'age': 25}, fluent.get_attributes())
