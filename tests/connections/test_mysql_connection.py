f04011a43074b956cdea4f17a87cda440f1e57ce
Sébastien Eustace <sebastien@eustace.io>
2016-09-26 23:18:28 -0500

    Fixes use_qmark information not being passed to schema grammars

---
+++ tests/connections/test_mysql_connection.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from .. import OratorTestCase
+
+from orator.connections.mysql_connection import MySQLConnection
+
+
+class MySQLConnectionTestCase(OratorTestCase):
+
+    def test_marker_is_properly_set(self):
+        connection = MySQLConnection(None, 'database', '', {'use_qmark': True})
+
+        self.assertEqual('?', connection.get_marker())
+
+    def test_marker_default(self):
+        connection = MySQLConnection(None, 'database', '', {})
+
+        self.assertIsNone(connection.get_marker())
+
+    def test_marker_use_qmark_false(self):
+        connection = MySQLConnection(None, 'database', '', {'use_qmark': False})
+
+        self.assertIsNone(connection.get_marker())
