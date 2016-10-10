e8211ed6e05f4652b77da784402c84da3b8ed216
Mathan  Kumar <mathankumar006@gmail.com>
2016-10-10 20:54:02 +0530

    Using multiple where conditions in a list in single where method  (#108)
    * Using multiple where conditions in a list in single where method as eloquent usage
    
    * Fixing the build
    
    * Fixing the build
    
    * Adding test cases for where list
    
    * Importing argumentError exception in test case

--- tests/query/test_query_builder.py
+++ tests/query/test_query_builder.py
@@ -7,6 +7,7 @@ from .. import mock
 
 from ..utils import MockConnection, MockProcessor
 
+from orator.exceptions import ArgumentError
 from orator.query.grammars import (
     QueryGrammar,
     PostgresQueryGrammar,
@@ -683,6 +684,30 @@ class QueryBuilderTestCase(OratorTestCase):
         )
         self.assertEqual([1, 'foo'], builder.get_bindings())
 
+    def test_multiple_wheres_in_list(self):
+        builder = self.get_builder()
+        builder.select('*').from_('users').where([['name', '=', 'bar'], ['age', '=', 25]])
+        self.assertEqual(
+            'SELECT * FROM "users" WHERE ("name" = ? AND "age" = ?)',
+            builder.to_sql()
+        )
+        self.assertEqual(['bar', 25], builder.get_bindings())
+
+    def test_multiple_wheres_in_list_with_exception(self):
+        builder = self.get_builder()
+        try:
+            builder.select('*').from_('users').where([['name', 'bar'], ['age', '=', 25]])
+            self.fail('Builder has not raised Argument Error for invalid no. of values in where list')
+        except ArgumentError:
+            self.assertTrue(True)
+
+        try:
+            builder.select('*').from_('users').where(['name', 'bar'])
+            self.fail('Builder has not raised Argument Error for invalid datatype in where list')
+        except ArgumentError:
+            self.assertTrue(True)
+
+
     def test_nested_wheres(self):
         builder = self.get_builder()
         builder.select('*').from_('users').where('email', '=', 'foo').or_where(
@@ -1210,7 +1235,7 @@ class QueryBuilderTestCase(OratorTestCase):
             query, ['foo', 'bar', 1]
         )
         self.assertEqual(1, result)
-        
+
         builder = self.get_mysql_builder()
         marker = builder.get_grammar().get_marker()
         query = 'UPDATE `users` SET `email` = %s, `name` = %s WHERE `id` = %s' % (marker, marker, marker)
