1db894aa7b992061c510d69cce2f22f6e8f7e43f
Chris Selmer <cmselmer@gmail.com>
2017-03-04 12:20:06 -0800

    Updates default location of seeders

--- orator/commands/migrations/refresh_command.py
+++ orator/commands/migrations/refresh_command.py
@@ -12,7 +12,7 @@ class RefreshCommand(BaseCommand):
         {--p|path= : The path of migrations files to be executed.}
         {--s|seed : Indicates if the seed task should be re-run.}
         {--seed-path= : The path of seeds files to be executed.
-                        Defaults to <comment>./seeders</comment>.}
+                        Defaults to <comment>./seeds</comment>.}
         {--seeder=database_seeder : The name of the root seeder.}
     """
 
