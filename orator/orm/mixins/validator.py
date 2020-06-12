90e67eca6016438ebe68a2301b90884d43f4b1c7
alfonsocv12 <alfonsocvu@gmail.com>
2020-06-12 13:18:16 -0600

    Fixing error on atributes

--- orator/orm/mixins/validator.py
+++ orator/orm/mixins/validator.py
@@ -1,9 +1,11 @@
 import re
 
 class Validator(object):
+    _validation_init = True
     __errors__ = {'code':200, 'errors':[]}
 
-    def validate(self, key, require=False, data_type=False,
+    @classmethod
+    def validate(cls, key, require=False, data_type=False,
             regex=False, custom_error=False, *args):
         '''
         Function dedicated to validate if a imput has some values
@@ -16,20 +18,24 @@ class Validator(object):
         param: *args *args: arguments to go with the function
         return: data response: We can return the value validated or None if the value doest correspont to the statements
         '''
-        value = getattr(self, key, None)
+        if cls._validation_init:
+            cls._errors()
+            cls._validation_init = False
+        value = getattr(cls, key, None)
         if require and not value:
-            self._handle_error('require', key, custom_error=custom_error, *args)
-            return self
+            cls._handle_error('require', key, custom_error=custom_error, *args)
+            return cls
         if data_type and not isinstance(value, data_type):
-            self._handle_error('data type', key, custom_msg='Bad data type on {}'.format(key),
+            cls._handle_error('data type', key, custom_msg='Bad data type on {}'.format(key),
                 custom_error=custom_error, *args)
-            return self
+            return cls
         if regex and not re.match(regex, value):
-            if not require: self.validate(key, require=True)
-            self._handle_error('regex', key, custom_error=custom_error, *args)
-        return self
+            if not require: cls.validate(key, require=True)
+            cls._handle_error('regex', key, custom_error=custom_error, *args)
+        return cls
 
-    def _handle_error(self, type_error, value_name,
+    @classmethod
+    def _handle_error(cls, type_error, value_name,
             custom_msg=False, custom_error=False, *args):
         '''
         Funtion dedicated to handle errors on the validation
@@ -43,9 +49,52 @@ class Validator(object):
         '''
         if custom_error:
             custom_error(*args)
-        self.__errors__['code'] = 400
-        self.__errors__['errors'].append({
-            'msg': custom_msg
+        cls._modify_errors(code=400, msg=custom_msg
                 if custom_msg else
                 'Error of {} on {}'.format(type_error, value_name)
-        })
+        )
+
+    def _change_error_code(self, code):
+        '''
+        Fucntion dedicated do _change_error_code
+
+        param: int code: The error code that we will return
+        '''
+        self._errors(code=code)
+
+    def errors(self, code=None, msg=None):
+        '''
+        Function dedicated to delete errors after use
+
+        return: __errors__
+        '''
+        self._validation_init = True
+        errors = self.__errors__
+        self.__errors__ = {'code':200, 'errors':[]}
+        return errors
+
+    @classmethod
+    def _errors(cls, code=None, msg=None):
+        '''
+        Function dedicated to delete errors after use
+
+        return: __errors__
+        '''
+        errors = cls.__errors__
+        cls.__errors__ = {'code':200, 'errors':[]}
+        return errors
+
+    @classmethod
+    def _modify_errors(cls, code=None, msg=None):
+        '''
+        Function dedicated to modify errors
+
+        param: int code: The error code that we will return
+        param: str msg: the msg to append to error list
+        '''
+        if code:
+            cls.__errors__['code'] = code
+        if msg:
+            cls.__errors__['errors'].append({
+                'msg': msg
+            })
